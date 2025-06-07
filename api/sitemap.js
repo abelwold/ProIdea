cc23cbe8f8988b9dc101a54675f168a1557b7caa
abelwold <abenerea@gmail.com>
2025-06-07 15:34:56 +0200

    � Serve sitemap with correct headers via API

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,11 +1,24 @@
 export default function handler(req, res) {
   res.setHeader('Content-Type', 'application/xml');
-  res.status(200).send(`
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url><loc>https://pro-idea-abels-projects-56f2149c.vercel.app/</loc><priority>1.0</priority></url>
-      <url><loc>https://pro-idea-abels-projects-56f2149c.vercel.app/projects</loc><priority>0.9</priority></url>
-      <url><loc>https://pro-idea-abels-projects-56f2149c.vercel.app/about</loc><priority>0.8</priority></url>
-      <url><loc>https://pro-idea-abels-projects-56f2149c.vercel.app/contact</loc><priority>0.8</priority></url>
-    </urlset>
-  `);
+  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://pro-idea-abels-projects-56f2149c.vercel.app/</loc>
+      <lastmod>2025-06-07</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>1.0</priority>
+    </url>
+    <url>
+      <loc>https://pro-idea-abels-projects-56f2149c.vercel.app/projects</loc>
+      <lastmod>2025-06-07</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>0.9</priority>
+    </url>
+    <url>
+      <loc>https://pro-idea-abels-projects-56f2149c.vercel.app/contact</loc>
+      <lastmod>2025-06-07</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>0.8</priority>
+    </url>
+  </urlset>`);
 }
